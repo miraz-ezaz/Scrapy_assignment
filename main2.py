5badeb3077d973f08fefe4c6836dfa6bb1582006
miraz-ezaz <miraz.ezaz@gmail.com>
2024-08-12 01:24:19 +0600

    remove unnecessary files

--- main2.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-
-page = requests.post('https://uk.trip.com/hotels/?locale=en-GB&curr=GBP')
-
-soup = BeautifulSoup(page.text, 'html.parser')
-
-print(soup.prettify())
